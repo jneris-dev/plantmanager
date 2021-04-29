21c484da407e174368649b08ec67810869332b06
jneris-wd <site@jneris.com.br>
2021-04-28 21:53:09 -0300

    endClass04

--- src/pages/MyPlants.tsx
+++ src/pages/MyPlants.tsx
@@ -6,19 +6,22 @@ import {
     Image,
 } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
+import { formatDistance } from 'date-fns';
+import { pt } from 'date-fns/locale';
 
 import { Header } from '../components/Header';
+import { PlantCardSecondary } from '../components/PlantCardSecondary';
 import { PlantProps, loadPlant } from '../libs/storage';
 
 import colors from '../styles/colors';
 import waterDrop from '../assets/waterdrop.png';
-import { formatDistance } from 'date-fns';
-import { pt } from 'date-fns/locale';
+import fonts from '../styles/fonts';
 
 export function MyPlants() {
     const [myPlants, setMyPlants] = useState<PlantProps[]>([]);
     const [loading, setLoading] = useState(true);
     const [nextWatered, setNextWatered] = useState<string>();
+
 
     useEffect(() => {
         async function loadStorageData() {
@@ -63,10 +66,12 @@ export function MyPlants() {
                     data={myPlants}
                     keyExtractor={(item) => String(item.id)}
                     renderItem={({ item }) => (
-                        <Text>Elemento</Text>
+                        <PlantCardSecondary
+                            data={item}
+                        />
                     )}
                     showsVerticalScrollIndicator={false}
-                    contentContainerStyle={{ flex: 1 }}
+                    onEndReachedThreshold={0.1}
                 />
             </View>
         </View>
@@ -82,18 +87,34 @@ const styles = StyleSheet.create({
         backgroundColor: colors.background,
     },
     spotlight: {
-
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: colors.blue_light,
+        borderRadius: 20,
+        padding: 20,
+        marginTop: 10,
     },
     spotlightImage: {
-
+        width: 50,
+        height: 50,
     },
     spotlightText: {
-
+        flex: 1,
+        color: colors.blue,
+        paddingLeft: 20,
+        fontFamily: fonts.text,
+        fontSize: 15,
     },
     plants: {
-
+        flex: 1,
+        width: '100%',
+        marginTop: 15,
     },
     plantsTitle: {
-
+        fontSize: 24,
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        marginVertical: 20,
     },
 })
--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -9,6 +9,7 @@ import { Confirmation } from '../pages/Confirmation';
 import { PlantSelect } from '../pages/PlantSelect';
 import { PlantSave } from '../pages/PantSave';
 import { MyPlants } from '../pages/MyPlants';
+import AuthRoutes from './tab.routes';
 
 const stackRoutes = createStackNavigator();
 
@@ -38,7 +39,7 @@ const AppRoutes: React.FC = () => (
 
         <stackRoutes.Screen
             name="PlantSelect"
-            component={PlantSelect}
+            component={AuthRoutes}
         />
 
         <stackRoutes.Screen
@@ -48,7 +49,7 @@ const AppRoutes: React.FC = () => (
 
         <stackRoutes.Screen
             name="MyPlants"
-            component={MyPlants}
+            component={AuthRoutes}
         />
     </stackRoutes.Navigator>
 )
